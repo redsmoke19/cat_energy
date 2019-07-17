4217beb2ee045341fed9b35767bf1767ebb20e36
redsmoke19 <44342965+redsmoke19@users.noreply.github.com>
2019-07-17 16:31:35 +0400

    Сделал меню мобильной версии сайта, начал делать блок с баннером

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
     svg4everybody({});
 
+  var mainNav = document.querySelector('.main-nav');
+  mainNav.classList.remove('main-nav--nojs');
+
     var sandwich = function () {
         $(document).on('click', '.sandwich', function () {
             $(this).toggleClass('sandwich--active');
-
+            $('.main-nav').toggleClass('main-nav--active');
         });
     };
+
     sandwich();
 
 });
