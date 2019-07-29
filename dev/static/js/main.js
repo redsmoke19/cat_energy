22817c90ee128533765bc14d2e1c2a46c998c4a4
redsmoke19 <44342965+redsmoke19@users.noreply.github.com>
2019-07-29 15:26:01 +0400

    Доработал блок сравнения. Пытаюсь разобраться с Google Maps на главной странице

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -4,13 +4,22 @@ $(document).ready(function () {
   var mainNav = document.querySelector('.main-nav');
   mainNav.classList.remove('main-nav--nojs');
 
-    var sandwich = function () {
-        $(document).on('click', '.sandwich', function () {
-            $(this).toggleClass('sandwich--active');
-            $('.main-nav').toggleClass('main-nav--active');
-        });
-    };
+  var sandwich = function () {
+      $(document).on('click', '.sandwich', function () {
+          $(this).toggleClass('sandwich--active');
+          $('.main-nav').toggleClass('main-nav--active');
+      });
+  };
 
-    sandwich();
+  function initMap() {
+    var coordinates = {lat: 59.938808, lng: 30.323026},
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: coordinates,
+          zoom: 14
+        });
+  }
+
+  sandwich();
+  initMap();
 
 });
