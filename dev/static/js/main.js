d869238b1e6fac8cedb029d8b31d187cf3306f69
redsmoke19 <44342965+redsmoke19@users.noreply.github.com>
2019-08-06 14:53:12 +0400

    Полность доделал весь адаптив главной странцы магазина

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -11,6 +11,28 @@ $(document).ready(function () {
       });
   };
 
+  var slickBefore = document.querySelector('.comparions__buttons--before');
+  var slickAfter = document.querySelector('.comparions__buttons--after');
+  var slick = document.querySelector('.comparions__slick');
+  var imageBefore = document.querySelector('.comparions__image--before');
+  var imageAfter = document.querySelector('.comparions__image--after');
+
+  slickBefore.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    slick.classList.remove('comparions__slick--after');
+    slick.classList.add('comparions__slick--before');
+    imageBefore.style.display = "";
+    imageAfter.style.display = "";
+  });
+
+  slickAfter.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    slick.classList.remove('comparions__slick--before');
+    slick.classList.add('comparions__slick--after');
+    imageBefore.style.display = "none";
+    imageAfter.style.display = "block";
+  })
+
   sandwich();
 
 });
